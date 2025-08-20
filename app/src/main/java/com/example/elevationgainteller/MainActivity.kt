544efc373e41730e95deac274bc89699e908d25a
Clément <clemm.favre@gmail.com>
2025-08-21 00:25:38 +0200

    first message appears

--- app/src/main/java/com/example/elevationgainteller/MainActivity.kt
+++ app/src/main/java/com/example/elevationgainteller/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.elevationgainteller.ui.theme.ElevationGainTellerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +25,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ElevationGainTellerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Description("How to use the app", "Press this to do that")
                 }
             }
         }
@@ -31,17 +37,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Description(message: String, secondText: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
+        Text(
+            text = message,
+            fontSize = 40.sp,
+            lineHeight = 100.sp
+        )
+        Text(
+            text = secondText,
+            fontSize = 20.sp,
+            lineHeight = 100.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DescriptionPreview() {
     ElevationGainTellerTheme {
-        Greeting("Android")
+        Description("How to use the app", "Press this to do that")
     }
 }
