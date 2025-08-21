cc12e25307c615eae93a19032b32eb801a4b8979
Clément <clemm.favre@gmail.com>
2025-08-21 02:41:02 +0200

    add background image component in a box

--- app/src/main/java/com/example/elevationgainteller/MainActivity.kt
+++ app/src/main/java/com/example/elevationgainteller/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.elevationgainteller
 
 import android.os.Bundle
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,8 +38,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Description(
-                        "How to use the app",
-                        "Press this to do that",
+                        "Welcome to ElevationGainTeller !",
+                        "Press on the Start button and start running ! Then press on the Round trip button every time you made one. Enjoy !",
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -44,25 +49,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Description(message: String, secondText: String, modifier: Modifier = Modifier) {
+fun Description(title: String, instructions: String, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(8.dp),
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = message,
-            fontSize = 40.sp,
-            lineHeight = 100.sp,
-            textAlign = TextAlign.Center
+            text = title,
+            fontSize = 20.sp,
+            lineHeight = 20.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
         )
         Text(
-            text = secondText,
-            fontSize = 20.sp,
-            lineHeight = 100.sp,
-            textAlign = TextAlign.Center,
+            text = instructions,
+            fontSize = 8.sp,
+            lineHeight = 15.sp,
+            textAlign = TextAlign.Start,
             modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.Start)
+        )
+    }
+}
+
+@Composable
+fun BackGround(title: String, instructions: String, modifier: Modifier = Modifier) {
+    val bgImage = painterResource(R.drawable.stairs)
+    Box(modifier) {
+        Image(
+            painter = bgImage,
+            contentDescription = null
+        )
+        Description(
+            title = title,
+            instructions = instructions,
+            modifier = Modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BackGroundPreview() {
+    ElevationGainTellerTheme {
+        BackGround(
+            "Welcome to ElevationGainTeller !",
+            "Press on the Start button and start running ! Then press on the Round trip button every time you made one. Enjoy !"
         )
     }
 }
@@ -72,8 +103,8 @@ fun Description(message: String, secondText: String, modifier: Modifier = Modifi
 fun DescriptionPreview() {
     ElevationGainTellerTheme {
         Description(
-            "How to use the app",
-            "Press this to do that",
+            "Welcome to ElevationGainTeller !",
+            "Press on the Start button and start running ! Then press on the Round trip button every time you made one. Enjoy !",
             modifier = Modifier.padding(8.dp)
         )
     }
